921967b321f9bf0051c18c2136bfe78f5e554269
UT18 <45535028+UT18@users.noreply.github.com>
2021-07-17 17:51:12 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  return (
+    <div style={{ margin: 10 }}>
+      <h3> Editor</h3>
+
+      <div><textarea style={{ height: 400, width: 1000 }}></textarea></div>
+      <br></br>
+      <button >Run</button>
+      <br></br>
+    </div>
+  );
+}
+
+export default App;
